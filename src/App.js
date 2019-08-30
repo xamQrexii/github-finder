c682f98f0d3471aca5c561144051fe7135c90e53
xamQrexii <usama1630@gmail.com>
2019-08-30 07:22:24 +0500

    Home page implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
 import About from './components/pages/About';
 import NavBar from './components/layout/NavBar';
-import Users from './components/users/Users';
 import User from './components/users/User';
-import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 
 import GithubState from './context/github/GithubState';
@@ -17,29 +16,24 @@ const App = () => {
   return (
     <GithubState>
       <AlertState>
-      <Router>
-        <div>
-          <NavBar title='Github Finder' icon='fab fa-github' />
-          <div className='container'>
-            <Alert />
-            <Switch>
-              <Route exact path='/' render={props => (
-                <Fragment>
-                  <Search />
-                  <Users />
-                </Fragment>
-              )} />
-              <Route exact path='/about' component={About} />
-              <Route exact path='/user/:login' render={props => (
-                <User
-                  {...props}
+        <Router>
+          <div>
+            <NavBar title='Github Finder' icon='fab fa-github' />
+            <div className='container'>
+              <Alert />
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/about' component={About} />
+                <Route exact path='/user/:login' render={props => (
+                  <User
+                    {...props}
+                  />
+                )}
                 />
-              )}
-              />
-            </Switch>
+              </Switch>
+            </div>
           </div>
-        </div>
-      </Router>
+        </Router>
       </AlertState>
     </GithubState>
   )
