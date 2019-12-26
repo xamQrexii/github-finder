f5be42bf98acb4ff47df836337b055bf426704e8
xamqrexii <usama1630@gmail.com>
2019-12-27 01:44:50 +0500

    Updates

---
+++ precache-manifest.06b5c721f36a5d54fd5c44bd2b9413e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08b52a3392051e90af5d3b22319d0cdd",
+    "url": "/github-finder/index.html"
+  },
+  {
+    "revision": "d952dcb616c5bec2a7e9",
+    "url": "/github-finder/static/css/main.65a3f7db.chunk.css"
+  },
+  {
+    "revision": "bf5ff3a298980ae03ceb",
+    "url": "/github-finder/static/js/2.0490c2f1.chunk.js"
+  },
+  {
+    "revision": "d952dcb616c5bec2a7e9",
+    "url": "/github-finder/static/js/main.cfe024f8.chunk.js"
+  },
+  {
+    "revision": "a06ceeb70725f86afdce",
+    "url": "/github-finder/static/js/runtime~main.cb74c248.js"
+  },
+  {
+    "revision": "75ae2ba25d3d06d5079a940b79114940",
+    "url": "/github-finder/static/media/spinner.75ae2ba2.gif"
+  }
+]);
